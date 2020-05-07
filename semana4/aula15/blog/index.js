e03a91f32437d195ddb8c1d771739f8aca5c62a3
Caio Teixeira <caio-teixeira@MacBook-Air-de-Caio.local>
2020-05-07 17:08:50 -0300

    modificado aula15

--- semana4/aula15/blog/index.js
+++ semana4/aula15/blog/index.js
@@ -1,18 +1,16 @@
-class post {
-    constructor (titulo, autor, conteudo) {
-        this.titulo = titulo
-        this.autor = autor
-        this.conteudo = conteudo
-    }
-}
-
 let arrayPosts = []
 
 function aoClicarCriarPost() {
     const tituloDoPost = document.getElementById("titulo-post")
     const autorDoPost = document.getElementById("autor-post")
     const conteudoDoPost = document.getElementById("conteudo-post")
-    const novoPost = new post(tituloDoPost.value, autorDoPost.value, conteudoDoPost.value)
+
+    const novoPost = {
+        titulo: tituloDoPost.value,
+        autor: autorDoPost.value,
+        conteudo: conteudoDoPost.value
+    }
+
     arrayPosts.push(novoPost)
     tituloDoPost.value = ""
     autorDoPost.value = ""
